73043c0a23371d6e90d2c282337069771f3f1711
João Matos <joaocarlosm00@gmail.com>
2022-12-24 00:25:23 -0500

    New MIMIC query

--- data/sql/mimic_cancer.sql
+++ data/sql/mimic_cancer.sql
@@ -5,6 +5,25 @@ SELECT DISTINCT
   , icu.gender
   , pat.anchor_age
   , icu.race
+  , CASE 
+      WHEN (
+         LOWER(icu.race) LIKE "%white%"
+      OR LOWER(icu.race) LIKE "%portuguese%" 
+      OR LOWER(icu.race) LIKE "%caucasian%" 
+      ) THEN "White"
+      WHEN (
+         LOWER(icu.race) LIKE "%black%"
+      OR LOWER(icu.race) LIKE "%african american%"
+      ) THEN "Black"
+      WHEN (
+         LOWER(icu.race) LIKE "%hispanic%"
+      OR LOWER(icu.race) LIKE "%south american%" 
+      ) THEN "Hispanic"
+      WHEN (
+         LOWER(icu.race) LIKE "%asian%"
+      ) THEN "Asian"
+      ELSE "Other"
+    END AS race_group
   , weight.weight_admit
   , adm.adm_type
   , adm.adm_elective
@@ -17,24 +36,248 @@ SELECT DISTINCT
   , icu.first_hosp_stay
   , icu.icustay_seq
   , icu.first_icu_stay
-  , s3.sepsis3 AS sepsis3
+  , CASE WHEN s3.sepsis3 IS TRUE THEN 1 ELSE 0 END AS sepsis3
   , charlson.charlson_comorbidity_index
   , sf.SOFA
   , oa.oasis AS OASIS
-  , InvasiveVent.InvasiveVent_hr AS mech_vent
-  , rrt.rrt
-  , (pressor.stay_id = icu.stay_id) AS vasopressor
-  , cancer.cancer_types
+  , CASE
+      WHEN InvasiveVent.InvasiveVent_hr IS NOT NULL
+      THEN 1
+      ELSE 0
+    END AS mech_vent
+
+  , CASE
+      WHEN rrt.rrt IS NOT NULL
+      THEN 1
+      ELSE 0
+    END AS rrt
+
+  , CASE 
+      WHEN (pressor.stay_id = icu.stay_id) IS NOT NULL
+      THEN 1
+      ELSE 0
+    END AS vasopressor
+
+  , CASE WHEN 
+      icd_codes LIKE "%C__%"
+      THEN 1
+      ELSE 0
+    END AS has_cancer
+
+  , CASE WHEN (
+         icd_codes LIKE "%C0%" 
+      OR icd_codes LIKE "%C1%"
+      OR icd_codes LIKE "%C2%"
+      OR icd_codes LIKE "%C3%" 
+      OR icd_codes LIKE "%C4%"
+      OR icd_codes LIKE "%C5%"
+      OR icd_codes LIKE "%C6%"
+      OR icd_codes LIKE "%C70%"
+      OR icd_codes LIKE "%C71%"
+      OR icd_codes LIKE "%C72%"
+      OR icd_codes LIKE "%C73%"
+      OR icd_codes LIKE "%C74%"
+      OR icd_codes LIKE "%C75%"
+      OR icd_codes LIKE "%C76%"
+  ) THEN 1
+    ELSE 0
+  END AS cat_solid
+
+  , CASE WHEN (
+         icd_codes LIKE "%C77%" 
+      OR icd_codes LIKE "%C7B%"
+      OR icd_codes LIKE "%C78%"
+      OR icd_codes LIKE "%C79%" 
+      OR icd_codes LIKE "%C79%"
+  ) THEN 1
+    ELSE 0
+  END AS cat_metastasized
+
+  , CASE WHEN (
+         icd_codes LIKE "%C8"  
+      OR icd_codes LIKE "%C90" 
+      OR icd_codes LIKE "%C91" 
+      OR icd_codes LIKE "%C92" 
+      OR icd_codes LIKE "%C93" 
+      OR icd_codes LIKE "%C94" 
+      OR icd_codes LIKE "%C95" 
+  ) THEN 1
+    ELSE 0
+  END AS cat_hematological
+
+  , CASE WHEN (
+       icd_codes LIKE "%C17%"
+    OR icd_codes LIKE "%C18%"
+    OR icd_codes LIKE "%C19%"
+    OR icd_codes LIKE "%C20%"
+    OR icd_codes LIKE "%C21%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_colon_rectal
+
+  , CASE WHEN (
+       icd_codes LIKE "%C22%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_liver_bd
+
+  , CASE WHEN (
+       icd_codes LIKE "%C25%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_pancreatic
+
+  , CASE WHEN (
+       icd_codes LIKE "%C34%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_lung_bronchus
+
+  , CASE WHEN (
+       icd_codes LIKE "%C43%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_melanoma
+
+  , CASE WHEN (
+       icd_codes LIKE "%C50%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_breast
+
+  , CASE WHEN (
+        icd_codes LIKE "%C53%"
+    OR  icd_codes LIKE "%C54%"
+    OR  icd_codes LIKE "%C55%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_endometrial
+
+  , CASE WHEN (
+       icd_codes LIKE "%C61%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_prostate
+
+  , CASE WHEN (
+       icd_codes LIKE "%C64%"
+    OR icd_codes LIKE "%C65%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_kidney
+
+  , CASE WHEN (
+       icd_codes LIKE "%C67%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_bladder
+
+  , CASE WHEN (
+       icd_codes LIKE "%C73%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_thyroid
+
+  , CASE WHEN (
+       icd_codes LIKE "%C82%"
+    OR icd_codes LIKE "%C83%"
+    OR icd_codes LIKE "%C84%"
+    OR icd_codes LIKE "%C85%"
+    OR icd_codes LIKE "%C86%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_nhl
+
+  , CASE WHEN (
+       icd_codes LIKE "%C91%"
+    OR icd_codes LIKE "%C92%"
+    OR icd_codes LIKE "%C93%"
+    OR icd_codes LIKE "%C94%"
+    OR icd_codes LIKE "%C95%"
+  ) THEN 1
+    ELSE 0
+  END AS loc_leukemia
+
+  , CASE WHEN (
+       icd_codes LIKE "%I10%"
+    OR icd_codes LIKE "%I11%"
+    OR icd_codes LIKE "%I12%"
+    OR icd_codes LIKE "%I13%"
+    OR icd_codes LIKE "%I14%"
+    OR icd_codes LIKE "%I15%"
+    OR icd_codes LIKE "%I16%"
+    OR icd_codes LIKE "%I70%"
+  ) THEN 1
+    ELSE 0
+  END AS comm_hypertension
+
+  , CASE WHEN (
+       icd_codes LIKE "%I50%"
+    OR icd_codes LIKE "%I110%"
+    OR icd_codes LIKE "%I27%"
+    OR icd_codes LIKE "%I42%"
+    OR icd_codes LIKE "%I43%"
+    OR icd_codes LIKE "%I517%"
+  ) THEN 1
+    ELSE 0
+  END AS comm_heart_failure
+
+  , CASE 
+      WHEN icd_codes LIKE "%N181%" THEN 1
+      WHEN icd_codes LIKE "%N182%" THEN 2
+      WHEN icd_codes LIKE "%N183%" THEN 3
+      WHEN icd_codes LIKE "%N184%" THEN 4
+      WHEN (
+           icd_codes LIKE "%N185%" 
+        OR icd_codes LIKE "%N186%"
+      )
+      THEN 5
+    ELSE 0
+  
+  END AS comm_ckd
+
+  , CASE WHEN (
+       icd_codes LIKE "%J41%"
+    OR icd_codes LIKE "%J42%"
+    OR icd_codes LIKE "%J43%"
+    OR icd_codes LIKE "%J44%"
+    OR icd_codes LIKE "%J45%"
+    OR icd_codes LIKE "%J46%"
+    OR icd_codes LIKE "%J47%"
+  ) THEN 1
+    ELSE 0
+  END AS comm_copd
+
+  , CASE WHEN 
+      icd_codes LIKE "%J841%"
+      THEN 1
+      ELSE 0
+  END AS comm_asthma
+
+  , CASE WHEN (
+         discharge_location = "DIED"
+      OR discharge_location = "HOSPICE"
+  ) THEN 1
+    ELSE 0
+  END AS mortality_in
+
+  , CASE WHEN (
+         discharge_location = "DIED"
+      OR discharge_location = "HOSPICE"
+      OR ABS(TIMESTAMP_DIFF(pat.dod,icu.icu_outtime,DAY)) <= 90
+  ) THEN 1
+    ELSE 0
+  END AS mortality_90
+  
 
 -- ICU stays
 FROM physionet-data.mimiciv_derived.icustay_detail
 AS icu 
 
 -- Sepsis Patients
-INNER JOIN physionet-data.mimiciv_derived.sepsis3
+LEFT JOIN physionet-data.mimiciv_derived.sepsis3
 AS s3
 ON s3.stay_id = icu.stay_id
-AND s3.sepsis3 = TRUE
 
 -- Age
 LEFT JOIN physionet-data.mimiciv_hosp.patients
@@ -139,17 +382,17 @@ LEFT JOIN (
 AS oa
 ON oa.stay_id = icu.stay_id
 
--- Cancer
+-- ICD codes
 LEFT JOIN(
   SELECT
       hadm_id
-    , STRING_AGG(icd_final) AS cancer_types
+    , STRING_AGG(icd_final) AS icd_codes
   FROM `protean-chassis-368116.icd_codes.diagnoses_icd10`
-  WHERE icd_final LIKE "C%"
   GROUP BY hadm_id
 )
-AS cancer
-ON cancer.hadm_id = icu.hadm_id
+AS icd
+ON icd.hadm_id = icu.hadm_id
 
-WHERE cancer.cancer_types IS NOT NULL
+--WHERE has_cancer = TRUE
+
 ORDER BY icu.subject_id
