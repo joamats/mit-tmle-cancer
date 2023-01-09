19e5e27beaf1edbaddba5c235771cac9d2da5ae0
João Matos <joaocarlosm00@gmail.com>
2023-01-09 15:34:34 -0500

    Make cohort definition together

--- src/cohort.py
+++ src/cohort.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+# MIMIC
 df0 = pd.read_csv("data/MIMIC.csv")
 print(f"{len(df0)} stays in the ICU")
 
@@ -17,3 +18,19 @@ print(f"Removed {len(df2) - len(df3)} non-first (relative) stays")
 print(f"{len(df3)} patients with active cancer, sepsis, and on first stay")
 
 df3.to_csv('data/cohort_MIMIC.csv')
+
+# eICU
+df0 = pd.read_csv("data/eICU.csv")
+print(f"200859 stays in the ICU")
+print(f"Removed {200859 - len(df0)} stays without sepsis")
+print(f"{len(df0)} sepsis stays")
+
+df1 = df0[df0.has_cancer == 1]
+print(f"Removed {len(df0) - len(df1)} stays without active cancer")
+print(f"{len(df1)} patients with active cancer and sepsis")
+
+df2 = df1[df1.unitvisitnumber == 1]
+print(f"Removed {len(df1) - len(df2)} non-first (relative) stays")
+print(f"{len(df2)} patients with active cancer, sepsis, and on first stay")
+
+df2.to_csv('data/cohort_eICU.csv')
